3d1a3a42f2435c85b4d726a8ef7381f1060897ee
korenshir <korenshir.gmail.com>
2021-01-06 03:12:57 +0200

    arguments+ back button+ navigation UI

--- app/src/main/java/com/example/secondchance/profileFragment.java
+++ app/src/main/java/com/example/secondchance/profileFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,8 @@ public class profileFragment extends Fragment {
 
         // Inflate the layout for this fragment
         View view =inflater.inflate(R.layout.fragment_profile, container, false);
+        String userID= profileFragmentArgs.fromBundle(getArguments()).getUserID();
+        Log.d("TAG","user id is:"+userID);
 
         Button newsFeedFromProfile= view.findViewById(R.id.homeFrom_profile);
         newsFeedFromProfile.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_profileFragment_pop));
@@ -27,11 +30,23 @@ public class profileFragment extends Fragment {
         profileToself.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_profileFragment_self));
 
         Button addAPostProfileBtn= view.findViewById(R.id.addApost_profile);
-        addAPostProfileBtn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_profileFragment_to_addPostFragment));
-
+        addAPostProfileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stID="123";
+                profileFragmentDirections.ActionProfileFragmentToAddPostFragment actionAdd = profileFragmentDirections.actionProfileFragmentToAddPostFragment(stID);
+                Navigation.findNavController(view).navigate(actionAdd);
+            }
+        });
         Button messagesFromProfile= view.findViewById(R.id.messagesFrom_profile);
-        messagesFromProfile.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_profileFragment_to_messagesFragment));
-
+        messagesFromProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stID="123";
+                profileFragmentDirections.ActionProfileFragmentToMessagesFragment actionMessages = profileFragmentDirections.actionProfileFragmentToMessagesFragment(stID);
+                Navigation.findNavController(view).navigate(actionMessages);
+            }
+        });
        /* ImageButton itemDetailsFromProfile= view.findViewById(R.id.itemDetailsFrom_profile);
         itemDetailsFromProfile.setOnClickListener(new View.OnClickListener() {
             @Override
