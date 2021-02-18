a86d60fce778c2a9c07b48d9a7ecac4c7be37f46
Gal Shaked <galsh1994@gmail.com>
2021-02-18 21:32:17 +0200

    add register,login,index,maps,someone's profile,edit item fragments

--- app/src/main/java/com/example/secondchance/IndexFragment.java
+++ app/src/main/java/com/example/secondchance/IndexFragment.java
@@ -2,10 +2,14 @@ package com.example.secondchance;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -14,6 +18,12 @@ import com.example.secondchance.Model.User;
 
 public class IndexFragment extends Fragment {
 
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,6 +38,12 @@ public class IndexFragment extends Fragment {
         GoToRegister.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_index_to_register));
 
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        menu.clear();
+        inflater.inflate(R.menu.index_menu,menu);
     }
 
     }
--- app/src/main/java/com/example/secondchance/LoginFragment.java
+++ app/src/main/java/com/example/secondchance/LoginFragment.java
@@ -2,10 +2,14 @@ package com.example.secondchance;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -14,7 +18,17 @@ import android.widget.TextView;
 
 public class LoginFragment extends Fragment {
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        menu.clear();
+        inflater.inflate(R.menu.login_menu,menu);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
