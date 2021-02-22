199b5b6ec0b9f71883dea3db1b68450243759e92
Nivo <magnivo11@gmail.com>
2021-02-22 11:11:12 +0200

    1

--- app/src/main/java/com/example/secondchance/Model/AppLocalDb.java
+++ app/src/main/java/com/example/secondchance/Model/AppLocalDb.java
@@ -15,7 +15,7 @@ import com.example.secondchance.MyApplicaion;
         static public AppLocalDbRepository db =
                 Room.databaseBuilder(MyApplicaion.context,
                         AppLocalDbRepository.class,
-                        "ContextDb6.db")
+                        "ContextDb7.db")
                         .fallbackToDestructiveMigration()
                         .build();
     };
