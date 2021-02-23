480ef903aa958fd92ce119cda41e6c9dd5bb06a9
Gal Shaked <galsh1994@gmail.com>
2021-02-23 19:21:49 +0200

    Revert "delete unnecessary fragments, hint all edit-texts and cover the password."
    This reverts commit 03718b87

---
+++ app/src/main/java/com/example/secondchance/messagesFragment.java
@@ -0,0 +1,41 @@
+package com.example.secondchance;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class messagesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =inflater.inflate(R.layout.fragment_messages, container, false);
+
+        String userID= profileFragmentArgs.fromBundle(getArguments()).getUserID();
+        Log.d("TAG","user id is:"+userID);
+
+ ////        Button privateMsgBtn = view.findViewById(R.id.private_m_button);
+//  ///      privateMsgBtn.setOnClickListener(new View.OnClickListener() {
+ //        Button privateMsgBtn = view.findViewById(R.id.private_m_button);
+//        privateMsgBtn.setOnClickListener(new View.OnClickListener() {
+ //        @Override
+//        public void onClick(View v) {
+//        Navigation.findNavController(v).navigate(R.id.action_messages_to_private_messages);
+//         }
+//        });
+ //
+//
+ 
+
+ 
+        return view;
+    }
+}
