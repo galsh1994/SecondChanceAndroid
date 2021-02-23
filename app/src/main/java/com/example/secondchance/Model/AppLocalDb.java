ad7e2021459d1df96d0d7c1378f2f2e9f58dc101
Gal Shaked <galsh1994@gmail.com>
2021-02-23 20:18:13 +0200

    hide the password and hint the edit text

--- app/src/main/java/com/example/secondchance/Model/AppLocalDb.java
+++ app/src/main/java/com/example/secondchance/Model/AppLocalDb.java
@@ -6,7 +6,7 @@ import androidx.room.RoomDatabase;
 
 import com.example.secondchance.MyApplicaion;
 
-@Database(entities = {User.class,Post.class}, version = 4)
+@Database(entities = {User.class,Post.class}, version = 5)
     abstract class AppLocalDbRepository extends RoomDatabase {
         public abstract UserDao userDao();
         public abstract PostDao postDao();
@@ -15,7 +15,7 @@ import com.example.secondchance.MyApplicaion;
         static public AppLocalDbRepository db =
                 Room.databaseBuilder(MyApplicaion.context,
                         AppLocalDbRepository.class,
-                        "ContextDb8.db")
+                        "ContextDb9.db")
                         .fallbackToDestructiveMigration()
                         .build();
     };
