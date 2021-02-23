59db2244203aca3dd81608942dc4989631f7c929
Nivo <magnivo11@gmail.com>
2021-02-23 11:39:24 +0200

    fix login message and profile list

--- app/src/main/java/com/example/secondchance/PostListViewModel.java
+++ app/src/main/java/com/example/secondchance/PostListViewModel.java
@@ -11,8 +11,25 @@ import java.util.List;
 
 public class PostListViewModel extends ViewModel {
     private LiveData<List<Post>> postList= Model.instance.getAllPosts();
+    private LiveData<List<Post>> userPosts;
+    private String userID;
 
     public LiveData<List<Post>> getPostList() {
         return postList;
+    }
+
+    public LiveData<List<Post>> getUserPosts(String id){
+        if(userID==null){
+            userID=id;
+            userPosts=Model.instance.getAllUserPost(id);
+            return userPosts;
+        }
+        if(!userID.equals(id)){
+            userID=id;
+            userPosts=Model.instance.getAllUserPost(id);
+            return userPosts;
+        }
+        return userPosts;
+
     }
 }
